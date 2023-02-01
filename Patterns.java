7c131f4419bab58ab9e47c441132491807d87920
KashishBiserwal <112905564+KashishBiserwal@users.noreply.github.com>
2023-02-01 15:39:45 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -2,7 +2,7 @@ package controlStatement;
 //PATTERNS
 public class Patterns {
     public static void main(String[] args) {
-        System.out.println("Pattern 1");
+        System.out.println("Pattern 1: Star Steps");
 
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
@@ -23,7 +23,7 @@ public class Patterns {
 
 
         System.out.println("");
-        System.out.println("Pattern 3");
+        System.out.println("Pattern 3: Number Steps");
 
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
@@ -33,29 +33,53 @@ public class Patterns {
         }
 
         System.out.println("");
-        System.out.println("Pattern 4");
+        System.out.println("Pattern 4: Filled Triangle");
+        int n = 5;
 
-        int s = 5;
-        for(int i = s; i >= 0; i--){
-            for(int j = 1; j <= i; j++ ){
+        for(int i = 1; i<=5; i++){
+            for(int j = 5; j >= i; j--){
                 System.out.print(" ");
             }
-            for(int k = 1; k<= s-i;k++){
+            for(int j =1; j<= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("");
+        System.out.println("Pattern 5: Right Half Diamond");
+
+        for(int i = 1; i <= 2*n; i++){
+            int cols = i > n ? 2*n-i : i;
+            for(int j = 1; j <= cols; j++){
                 System.out.print("* ");
             }
             System.out.println(" ");
         }
 
         System.out.println("");
-        System.out.println("Pattern 5");
+        System.out.println("Pattern 6: Diamond");
 
-        int n = 5;
-        for(int i = 0; i < 2*n; i++){
-            int cols = i > n ? 2*n-i : i;
-            for(int j = 0; j < cols; j++){
+        for(int i = 1; i <= 2*n; i++){
+            int spaces = i < n ? n-i+1 : i-n+1;
+            for(int k= 1; k<= spaces; k++){
+                System.out.print(" ");
+            }
+            int cols = i < n ? i : 2*n-i;
+            for(int j = 1; j <= cols; j++){
                 System.out.print("* ");
             }
             System.out.println(" ");
+        }
+
+        System.out.println("");
+        System.out.println("Pattern 7");
+
+        for(int i = 1; i<=5; i++){
+            for(int j = 1; j<=5; j++){
+                System.out.print(i*j + " ");
+            }
+            System.out.println();
         }
     }
 }
